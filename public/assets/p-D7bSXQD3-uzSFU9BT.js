c31f03f5ae39131b9d8c1b27a36c362c57ba6128
eslamghazi <eslamghazi20002@gmail.com>
2026-09-12 14:40:12 +0300

    release: production build 2026-09-12 11:40:12

--- public/assets/p-D7bSXQD3-uzSFU9BT.js
+++ public/assets/p-D7bSXQD3-uzSFU9BT.js
@@ -1,3 +1,3 @@
-import{d as r,m as s,e as i,P as a,i as d}from"./index-De4WCxtt.js";/*!
+import{d as r,m as s,e as i,P as a,i as d}from"./index-2YX2CR-n.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const c=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const n=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!n)return;const t=s(n);t&&new Promise(o=>i(t,o)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await d(t,300),t.style.removeProperty("--overflow")})})})})};export{c as startStatusTap};
